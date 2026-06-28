30942eb02f03bafa39ab41d06d136d7730319aa4
UnpolishedNoob <banikpritom321@gmail.com>
2026-06-29 05:14:40 +0600

    Design patch routes for user role and status change

--- modules/admin/admin.controller.js
+++ modules/admin/admin.controller.js
@@ -6,6 +6,11 @@ function getCollection() {
     return db.collection('blood_requests');
 }
 
+function getUserCollection() {
+    const db = database.getDb();
+    return db.collection('user');
+}
+
 async function getAllRequests(req, res) {
     try {
         const collection = getCollection();
@@ -13,13 +18,13 @@ async function getAllRequests(req, res) {
         const limit = parseInt(req.query.limit) || 5;
         const offset = (page - 1) * limit;
         const query = {};
-        const {role}=req.body;
-        if(role!="admin"){
+        const { role } = req.body;
+        if (role != "admin") {
             return res.status(403).json({ error: "Forbidden api" });
         }
 
         // 2. Only add status to the filter if it was actually passed in the request
-        if (req.query.status!="all") {
+        if (req.query.status != "all") {
             query.status = req.query.status;
         }
 
@@ -45,5 +50,108 @@ async function getAllRequests(req, res) {
     }
 }
 
+async function getAllUsers(req, res) {
+    try {
+        const collection = getUserCollection();
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 5;
+        const offset = (page - 1) * limit;
+        const query = {};
+        const { role } = req.body;
+        if (role != "admin") {
+            return res.status(403).json({ error: "Forbidden api" });
+        }
 
-module.exports = { getAllRequests }
+        // 2. Only add status to the filter if it was actually passed in the request
+        if (req.query.status != "all") {
+            query.status = req.query.status;
+        }
+
+        const requests = await collection.find(query)
+            .sort({ updatedAt: -1 })
+            .skip(offset)
+            .limit(limit)
+            .toArray();
+
+        const totalRequests = await collection.countDocuments(query);
+
+        if (requests.length === 0) {
+            return res.status(404).json({ message: "No blood requests found for this user." });
+        }
+        res.status(200).json({
+            users: requests,
+            total: totalRequests,
+            page: page,
+            limit: limit
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+async function changeUserStatus(req, res) {
+    try {
+        const collection = getUserCollection();
+        const requestId = req.params.id;
+
+        const now = new Date();
+
+        const { status, me } = req.body;
+
+        if (me != "admin") {
+            return res.status(403).json({ message: "Unauthorized" })
+        }
+
+        const result = await collection.updateOne(
+            { _id: new ObjectId(requestId) },
+            {
+                $set: {
+                    status: status,
+                    updatedAt: now
+                }
+            }
+        );
+        if (result.matchedCount === 0) {
+            return res.status(404).json({ message: "User not found." });
+        }
+
+        res.status(200).json({ message: "Request status updated successfully." });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+async function changeUserRole(req,res) {
+    try {
+        const collection = getUserCollection();
+        const requestId = req.params.id;
+
+        const now = new Date();
+
+        const { role, me } = req.body;
+
+        if (me != "admin") {
+            return res.status(403).json({ message: "Unauthorized" })
+        }
+
+        const result = await collection.updateOne(
+            { _id: new ObjectId(requestId) },
+            {
+                $set: {
+                    role: role,
+                    updatedAt: now
+                }
+            }
+        );
+        if (result.matchedCount === 0) {
+            return res.status(404).json({ message: "User not found." });
+        }
+
+        res.status(200).json({ message: "Request status updated successfully." });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+
+module.exports = { getAllRequests, getAllUsers, changeUserStatus, changeUserRole }
--- modules/admin/admin.routes.js
+++ modules/admin/admin.routes.js
@@ -6,6 +6,12 @@ const adminController=require("./admin.controller")
 
 router.post("/admin/get-all-requests",adminController.getAllRequests);
 
+router.post("/admin/get-all-users",adminController.getAllUsers);
+
+router.patch("/admin/user-status-change/:id",adminController.changeUserStatus);
+
+router.patch("/admin/change-user-role/:id",adminController.changeUserRole);
+
 
 
 module.exports = router;
