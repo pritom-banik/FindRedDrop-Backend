f6e94f5b7bfa48a9d4a76e4e32313da46a3a2ce6
UnpolishedNoob <banikpritom321@gmail.com>
2026-06-28 17:32:15 +0600

    add pagination for get request by user id api

--- modules/userinfo/user.controller.js
+++ modules/userinfo/user.controller.js
@@ -6,20 +6,34 @@ function getCollection() {
     return db.collection('blood_requests');
 }
 
-async function getUserRequestById(req,res) {
-    const userId =  req.params.id;
+async function getUserRequestById(req, res) {
+    const userId = req.params.id;
     try {
         const collection = getCollection();
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 5;
+        const offset = (page - 1) * limit;
+
         const requests = await collection.find({ requesterId: userId })
-        .sort({updatedAt:-1})
-        .toArray();
+            .sort({ updatedAt: -1 })
+            .skip(offset)
+            .limit(limit)
+            .toArray();
+
+        const totalRequests = await collection.countDocuments({ requesterId: userId });
+
         if (requests.length === 0) {
             return res.status(404).json({ message: "No blood requests found for this user." });
         }
-        res.status(200).json(requests);
+        res.status(200).json({
+            requests: requests,
+            total: totalRequests,
+            page: page,
+            limit: limit
+        });
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 }
 
-module.exports={getUserRequestById}
+module.exports = { getUserRequestById }
