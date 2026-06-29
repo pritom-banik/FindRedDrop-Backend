da7428f1b6619f1459cd6733a117d93098871f46
UnpolishedNoob <banikpritom321@gmail.com>
2026-06-29 23:37:42 +0600

    Set middleware in all admin routes

--- modules/admin/admin.routes.js
+++ modules/admin/admin.routes.js
@@ -3,22 +3,24 @@ const router=express.Router()
 
 const adminController=require("./admin.controller")
 
+const middllewire=require("../../middlewire/middlewire")
 
-router.post("/admin/get-all-requests",adminController.getAllRequests);
 
-router.post("/admin/get-all-users",adminController.getAllUsers);
+router.post("/admin/get-all-requests",middllewire.verifyToken,adminController.getAllRequests);
 
-router.patch("/admin/user-status-change/:id",adminController.changeUserStatus);
+router.post("/admin/get-all-users",middllewire.verifyToken,adminController.getAllUsers);
 
-router.patch("/admin/change-user-role/:id",adminController.changeUserRole);
+router.patch("/admin/user-status-change/:id",middllewire.verifyToken,adminController.changeUserStatus);
+
+router.patch("/admin/change-user-role/:id",middllewire.verifyToken,adminController.changeUserRole);
 
 //All analytical route
 
-router.get("/admin/get-total-user",adminController.getTotalUser)
+router.get("/admin/get-total-user",middllewire.verifyToken,adminController.getTotalUser)
 
-router.get("/admin/get-total-funding",adminController.getTotalFunding)
+router.get("/admin/get-total-funding",middllewire.verifyToken,adminController.getTotalFunding)
 
-router.get("/admin/get-total-blood-request-info",adminController.getTotalBloodReqInfo)
+router.get("/admin/get-total-blood-request-info",middllewire.verifyToken,adminController.getTotalBloodReqInfo)
 
 
 
